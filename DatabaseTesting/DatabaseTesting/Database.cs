fc8a1aa7a9082872635024ab63fe5a446ef49659
Christian Nytra <ch.nytra.13@gmail.com>
2019-01-07 12:31:53 +0100

    initial commit

---
+++ DatabaseTesting/DatabaseTesting/Database.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseTesting
+{
+    public class Database
+    {
+        public int[] DbArray;
+
+        public Database()
+        {
+            DbArray = new int[] { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
+        }
+
+        public void Add()
+        {
+
+        }
+    }
+}
---
+++ DatabaseTesting/Tests/DBTest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DatabaseTesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class DBTest
+    {
+        [TestMethod]
+        public void ArrayLengthTest()
+        {
+            // Arrange
+            var dbArrayLengthExpected = 16;
+
+            // Act
+            var dBinit = new Database();
+            var dbArrayLengthActual = dBinit.DbArray.Length;
+
+            // Assert
+            Assert.AreEqual(dbArrayLengthExpected, dbArrayLengthActual);
+        }
+
+        [TestMethod]
+        public void TestAddMethod()
+        {
+            // Arrange
+            var dbInit = new Database();
+
+            // Act
+            dbInit.Add();
+
+            // Assert
+            Assert.ThrowsException<InvalidOperationException>(null);
+        }
+
+        [TestMethod]
+        public void TestRemoveMethod()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+    }
+}
